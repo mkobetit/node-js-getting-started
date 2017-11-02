c72f2418e4c86b256866f8fc945f358a161e4afd
Mikey <michael@kobetitsch.com>
2017-11-02 19:29:38 -0400

    First Commit

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/index/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  
 });
+
